77abbd3fe1ef5adcffa5d52e17dd033e2473f7a9
whatprizrak <mr.priz1980@gmail.com>
2026-09-16 07:29:48 +0000

    blablabla

---
+++ src/components/Layout.jsx
@@ -0,0 +1,14 @@
+import Header from "./Header"
+import { Outlet } from "react-router-dom"
+
+const Layout = () => {
+    return (
+        <>
+            <Header />
+            <main className="page-area">
+                <Outlet />
+            </main>
+        </>
+    )
+}
+export default Layout
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,16 @@
 import ProductCard from '../components/ProductCard.jsx';
+import { products } from '../data/products.js'
+import { Link } from 'react-router-dom'
 
-export default function HomePage({ featuredProducts, onSelectProduct, onGoToCatalog }) {
+export default function HomePage() {
+  const featuredProducts = products.slice(0, 3)
   return (
     <section className="page-shell">
       <div className="hero">
         <div>
-          <h1>Растения, которые правда выживут у вас дома</h1>
-          <p>Подбор простых в уходе растений для новичков — от суккулентов до крупных фикусов.</p>
-          <button type="button" className="btn" onClick={onGoToCatalog}>Смотреть каталог</button>
+          <h1>А мне больше нравится Хаммам облизывать</h1>
+          <p>облизывайте, облизывайте, а я пока примусь за массажные столы</p>
+          <Link to="/catalog" type="button" className="btn">Смотреть Табакошку(СМЕРТЬ В АУТИЗМЕ)</Link>
         </div>
         <div className="hero-plant" />
       </div>
@@ -15,7 +18,7 @@ export default function HomePage({ featuredProducts, onSelectProduct, onGoToCata
       <p className="section-label">Популярное сейчас</p>
       <div className="product-grid">
         {featuredProducts.map(product => (
-          <ProductCard key={product.id} product={product} onSelect={onSelectProduct} />
+          <ProductCard key={product.id} product={product} />
         ))}
       </div>
     </section>
